ed16d45dd5226ce8fd76628ec02512761c40e41d
wcuadrosuniandes <w.cuadrosr@uniandes.edu.co>
2026-06-03 20:16:28 -0500

    Fix scroll position on route changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import { Toaster } from "@/components/ui/toaster"
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 // Add page imports here
 import Home from './pages/Home';
 import Lavadoras from './pages/Lavadoras';
 import LavadoraDetalle from './pages/LavadoraDetalle';
 
+function ScrollToTop() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+}
+
 function App() {
     return (
         <>
             <Router>
+                <ScrollToTop />
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/lavadoras" element={<Lavadoras />} />
